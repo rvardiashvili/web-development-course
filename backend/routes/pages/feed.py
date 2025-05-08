c5160d82556ffeb47096286ff446fba12a29c04e
rvardiashvili <vardiashvilirati33@gmail.com>
2025-05-08 14:54:30 +0200

    modified feed page, added community tabs

--- backend/routes/pages/feed.py
+++ backend/routes/pages/feed.py
@@ -5,3 +5,7 @@ feed_bp = Blueprint('feed', __name__, url_prefix='/feed')
 @feed_bp.route('/')
 def feed():
     return render_template('feed.html')
+
+@feed_bp.route('/community')
+def community():
+    return render_template('components/community.html')
--- frontend/static/js/posts.js
+++ frontend/static/js/posts.js
@@ -1,13 +1,15 @@
-postTypeSelector = document.querySelectorAll(".post-type-button");
 
-active = document.getElementById("All");
+export function initPostTypeButtons() {
+    const postTypeSelector = document.querySelectorAll(".post-type-button");
+    let active = document.getElementById("All");
 
-postTypeSelector.forEach((type) => {
-    type.addEventListener("click", function(){
-        type.style.border = ("solid black 2px");
-        active.style.border = ("none");
-        active = type;
+    postTypeSelector.forEach((type) => {
+        type.addEventListener("click", function () {
+            if (active) active.style.border = "none";
+            type.style.border = "solid black 2px";
+            active = type;
+        });
     });
-    
-});
+}
 
+initPostTypeButtons();
